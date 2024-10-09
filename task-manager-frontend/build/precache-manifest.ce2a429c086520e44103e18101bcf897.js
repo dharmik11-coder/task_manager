704be05c97581ea61c2d4d312b7fc821876b8734
dharmik11-coder <mailto.dharmik11@gmail.com>
2024-10-08 21:18:48 -0400

    Add build folder for deployment

---
+++ task-manager-frontend/build/precache-manifest.ce2a429c086520e44103e18101bcf897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba435bcd525ba8c1239492555e465aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6b0b5afb668750446e4",
+    "url": "/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "161426e019a9dcff769c",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "a6b0b5afb668750446e4",
+    "url": "/static/js/2.4bdce0e8.chunk.js"
+  },
+  {
+    "revision": "161426e019a9dcff769c",
+    "url": "/static/js/main.efa767b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
